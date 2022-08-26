646f12cbded8e4726f26a59b2c3b095dc17fd990
Yan Batista <Yan.Batista@tutiplast.local>
2022-08-26 10:17:07 -0400

    chore: implements prisma mysql

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: ['standard-with-typescript', 'prettier'],
   parserOptions: {
     project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
   },
   rules: {
     // disable the rule for all files
