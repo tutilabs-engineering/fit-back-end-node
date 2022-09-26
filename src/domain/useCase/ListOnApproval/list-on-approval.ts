3d7ba8a661a8dff42bd4787e889ab297512fbf99
Yan Batista <Yan.Batista@tutiplast.local>
2022-09-26 09:25:38 -0400

    chore: fixed some errors

--- src/domain/useCase/ListOnApproval/list-on-approval.ts
+++ src/domain/useCase/ListOnApproval/list-on-approval.ts
@@ -1,13 +1,10 @@
 import { FitModel } from '../models/fit'
 
 export interface ListOnApproval {
-  execute: () => Promise<ListOnApproval.Result>
+  execute: () => Promise<ListOnApproval.Result[]>
 }
 
 export namespace ListOnApproval {
   export type Params = FitModel
-  export type Result = Omit<
-    FitModel,
-    'Attention_point_control' | 'workstations'
-  >
+  export type Result = any
 }
--- src/infra/repositories/data/fit/list-on-approval-repository.ts
+++ src/infra/repositories/data/fit/list-on-approval-repository.ts
@@ -1,7 +1,7 @@
 import { ListOnApproval } from '../../../../domain/useCase/ListOnApproval/list-on-approval'
 
 export interface ListOnApprovalRepository {
-  execute: () => Promise<ListOnApprovalRepository.Result>
+  execute: () => Promise<ListOnApprovalRepository.Result[]>
 }
 
 export namespace ListOnApprovalRepository {
