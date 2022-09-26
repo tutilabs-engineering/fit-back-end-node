b1c89761f29e4a0bd0e904d59934aca8ad939521
rafael almeida <rafael.almeida@tutiplast.local>
2022-09-26 14:10:25 -0400

    chore: firts release 01

--- src/main/factories/usecases/add-fit-factory copy.ts
+++
@@ -1,8 +0,0 @@
-import { AddFit } from '../../../domain/useCase/Add/add-fit'
-import { FitMysqlRepository } from '../../../infra/data-mysql/fit-mysql-repository'
-import { DbAddFit } from '../../../infra/implements/usecase/db-add-fit'
-
-export const makeDbAddFit = (): AddFit => {
-  const fitMysqlRepository = new FitMysqlRepository()
-  return new DbAddFit(fitMysqlRepository)
-}
